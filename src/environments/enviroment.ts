b056810e4d2c78fe07b3fb74c0262ee0cbed90cc
Yosefnago <yosinago5@gmail.com>
2025-11-15 16:06:33 +0200

    Stop tracking environments folder

--- src/environments/enviroment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-
-  // EmailJS Configuration
-  emailServiceId: 'service_ypopxtk',       
-  emailTemplateId: 'template_97psbwk',     
-  emailPublicKey: 'public_v_gp9zH4j4mDhUT-_' 
-};
